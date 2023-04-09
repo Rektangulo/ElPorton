011cea58cc560a8cc4d84adbbb2848944af5e5e3
Rektangulo <danielalex.rex@gmail.com>
2023-04-09 02:39:39 +0200

    Added template for the dashboard pages

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -1,16 +1,13 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+    	<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	  <!-- bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  </head>
-  <body>
-	  @yield('content')
-	 <!-- bootstrap scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-	  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-  </body>
+    	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	</head>
+	<body>
+	  <!-- bootstrap scripts -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	</body>
 </html>
---
+++ resources/views/dashboard/layout.blade.php
@@ -0,0 +1,34 @@
+@extends('bootstrap')
+<body class="bg-dark text-white">
+	<nav class="navbar navbar-expand-lg navbar-dark fs-4" style="background-color: #400b96;">
+	  <div class="container-fluid">
+		<a class="navbar-brand fs-3" href="#">Dashboard</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+		  <ul class="navbar-nav">
+			<li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+				Resources
+			  </a>
+			  <ul class="dropdown-menu dropdown-menu-dark">
+				<li><a class="dropdown-item" href="#">Users</a></li>
+				<li><a class="dropdown-item" href="#">Menus</a></li>
+				<li><a class="dropdown-item" href="#">Tables</a></li>
+			  </ul>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link me-2" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link me-2" href="#">Reservations</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link me-2" href="#">Events</a>
+			</li>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+</body>
